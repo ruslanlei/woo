ed0f17406dc3b153b068ba68b51a43185d82495e
Ruslan Lei <rusya.lay@gmail.com>
2020-12-14 18:52:53 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <component :is="layout">
+    <router-view/>
+  </component>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import MainLayout from '@/layouts/MainLayout.vue';
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+export default {
+  name: 'App',
+  components: {
+    MainLayout,
+  },
+  computed: {
+    layout() {
+      return this.$route.meta.layout || 'mainLayout';
+    },
+  },
+};
+</script>
